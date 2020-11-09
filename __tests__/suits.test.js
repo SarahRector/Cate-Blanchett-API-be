20760538a9e9a6408b575a4c61e1b9c4be84d547
SarahRector <rector.sarah.r@gmail.com>
2020-11-08 18:40:22 -0800

    added get all route and test

--- __tests__/suits.test.js
+++ __tests__/suits.test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
-//const Suit = require('../lib/models/Suit');
+const Suit = require('../lib/models/Suit');
 
 describe('Cate-Blanchett-API-be routes', () => {
   beforeEach(() => {
@@ -24,6 +24,20 @@ describe('Cate-Blanchett-API-be routes', () => {
           designer: 'Fendi',
           image: 'test.url'
         });
+      });
+  });
+
+  it('finds all suits via GET', async() => {
+    const suits = await Promise.all([
+      Suit.insert({ color: 'yellow', designer: 'Fendi', image: 'test1.png' }),
+      Suit.insert({ color: 'green velvet', designer: 'Designio', image: 'test2.png' }),
+      Suit.insert({ color: 'purple', designer: 'Much Fashion', image: 'test3.png' }),
+    ]);
+
+    return request(app)
+      .get('/api/v1/suits')
+      .then(res => {
+        expect(res.body).toEqual(expect.arrayContaining(suits));
       });
   });
 });
